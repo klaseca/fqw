d441e13380a3275965474e7a91a1770ddf00e777
Maxim Molochkov <klaseca@gmail.com>
2020-05-31 14:39:06 +0400

    Edited code according to style guide

--- client/src/components/Main/WhyWe/WhyWeBox.jsx
+++ client/src/components/Main/WhyWe/WhyWeBox.jsx
@@ -8,30 +8,30 @@ export default function WhyWeBox() {
   const whyWeData = [
     {
       id: 'qwe',
-      title: 'zxcz',
-      icon: 'Engineer'
+      title: 'Опытные специалисты',
+      icon: 'Engineer',
     },
     {
       id: 'asd',
-      title: 'zxczxc',
-      icon: 'Money'
+      title: 'Доступные цены',
+      icon: 'Money',
     },
     {
       id: 'zxc',
-      title: 'zxczxc',
-      icon: 'Cabinet'
+      title: 'Онлайн запись',
+      icon: 'Cabinet',
     },
     {
       id: 'cvb',
-      title: 'zxczxc',
-      icon: 'Equip'
-    }
+      title: 'Современное оборудование',
+      icon: 'Equip',
+    },
   ];
 
   return (
     <Container maxWidth='lg'>
       <Grid container spacing={4}>
-        {whyWeData.map(item => (
+        {whyWeData.map((item) => (
           <Grid xs={12} lg={3} item key={item.id}>
             <WhyWeCard {...item} />
           </Grid>
--- client/src/components/Main/WhyWe/WhyWeCard.jsx
+++ client/src/components/Main/WhyWe/WhyWeCard.jsx
@@ -14,19 +14,20 @@ const Card = styled('div')({
   backgroundColor: '#ececec',
   padding: '20px',
   borderRadius: '5px',
-  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)'
+  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
 });
 
 const Title = styled('div')({
   display: 'flex',
   justifyContent: 'center',
   paddingTop: '10px',
-  fontSize: '2em'
+  fontSize: '2em',
+  textAlign: 'center',
 });
 
 const IconBox = styled('div')({
   display: 'flex',
-  minHeight: '50px'
+  minHeight: '50px',
 });
 
 export default function WhyWeCard({ title, icon }) {
