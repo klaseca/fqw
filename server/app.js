8ad94b8d5356a0fea2b2ad4245ae32c36fde39c2
Maxim Molochkov <klaseca@gmail.com>
2020-05-31 15:40:40 +0400

    Added passport and api

--- server/app.js
+++ server/app.js
@@ -1,5 +1,18 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const session = require('koa-session2');
+const passport = require('koa-passport');
+const apiRouter = require('./routes/api/apiRouter');
+require('./config/passport');
 
 const app = new Koa();
+
+app.use(bodyParser());
+app.use(session({ key: 'SESSIONID' }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(apiRouter.routes(), apiRouter.allowedMethods());
 
 module.exports = app;
